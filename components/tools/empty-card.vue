ed407d6cad48afc5940eedf23373e7a634059022
wenjuGao <wenjugao3@gmail.com>
2023-10-23 10:31:44 +0800

    fix:详情页

---
+++ components/tools/empty-card.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="empty">
+		<slot name="image">
+			<img class="w-28 h-28"
+				 src="https://qiniu.gaowenju.com/app-site/empty.png" />
+		</slot>
+		<div class="mt-8  gray-400 text-sm">{{ props.emptyText }}</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	emptyText: {
+		type: String,
+		default: 'woos! 未查询到你要查看的内容'
+	}
+})
+</script>
+
+<style lang="postcss">
+.empty {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	width: 100%;
+	@apply mt-20;
+}
+</style>
---
+++ components/tools/loading-card.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="loading">
+		<slot name="image">
+			<span class="loading loading-ring loading-lg"></span>
+		</slot>
+		<div class="loading-text">{{ props.loadingText }}</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	loadingText: {
+		type: String,
+		default: '加载中...'
+	}
+})
+
+</script>
