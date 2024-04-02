b4e1b7c40b5a6f3a3ee92307a56e1b44bc11f07b
小书童 <2213833089@qq.com>
2024-04-02 19:41:56 +0800

    no message

--- .nuxt/types/nitro-routes.d.ts
+++ .nuxt/types/nitro-routes.d.ts
@@ -24,6 +24,9 @@ declare module 'nitropack' {
     '/api/qiniu-preview': {
       'default': Simplify<Serialize<Awaited<ReturnType<typeof import('../../server/api/qiniu-preview').default>>>>
     }
+    '/api/test': {
+      'get': Simplify<Serialize<Awaited<ReturnType<typeof import('../../server/api/test.get').default>>>>
+    }
     '/__nuxt_error': {
       'default': Simplify<Serialize<Awaited<ReturnType<typeof import('../../node_modules/.pnpm/nuxt@3.11.1_@types+node@18.19.26_@unocss+reset@0.58.6_floating-vue@5.2.2_rollup@3.29.4_typesc_f4ybvezt55use5n3ldxgxqrtky/node_modules/nuxt/dist/core/runtime/nitro/renderer').default>>>>
     }
---
+++ server/api/test.get.ts
@@ -0,0 +1,26 @@
+import qiniu from "qiniu"
+const baseUrl = process.env.NUXT_QINIU_BASE_URL || ''
+const accessKey = process.env.NUXT_QINIU_ACCESS_KEY
+const secretKey = process.env.NUXT_QINIU_SECRET_KEY
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const config: any = new qiniu.conf.Config()
+config.zone = qiniu.zone.Zone_z0
+const bucket = new qiniu.rs.BucketManager(mac, config)
+
+export default defineEventHandler(async (event) => {
+	const key = decodeURIComponent(getQuery(event).key + '') || ''
+	// @ts-ignore
+	const deadline = parseInt(Date.now() / 1000) + 3600;
+	const file = /jpg|jpeg|png/.test(key) && !/watermark/.test(key) ? `${key}-watermark` : key
+	if (file && file !== undefined) {
+		return {
+			file,
+			qiniuUrl: bucket.privateDownloadUrl(baseUrl, file, deadline)
+		}
+	} else {
+		return createError({
+			statusCode: 400,
+			statusMessage: '图床sdk请求错误❎',
+		})
+	}
+})
